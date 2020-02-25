cb4e07f0369abe28d1ac704a6e6bc595b8c2f5ee
pl2005t <pl2005t@163.com>
2020-02-25 23:57:05 +0800

    souhu爬虫，实验PhantomJS

---
+++ souhu.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+driver=webdriver.PhantomJS(executable_path=r'D:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+driver.get('http://news.sohu.com/scroll/')
+print(driver.find_element_by_class_name('title').text)
