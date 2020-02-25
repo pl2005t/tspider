8758a9c4726335b0b1b947930511912134efae92
pl2005t <pl2005t@163.com>
2020-02-26 00:03:15 +0800

    更改文件路径

--- souhu.py
+++ souhu.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
-driver=webdriver.PhantomJS(executable_path=r'D:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+driver=webdriver.PhantomJS(executable_path=r'D:\soft\phantomjs-2.1.1-windows\bin\phantomjs.exe')
 driver.get('http://news.sohu.com/scroll/')
 print(driver.find_element_by_class_name('title').text)
